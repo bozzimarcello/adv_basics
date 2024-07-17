20392fdaeb930faeb932714b9b8bd09e62c1f318
Marcello Bozzi <bozzimarcello@gmail.com>
2024-07-17 14:10:12 +0200

    code created in the first course session

---
+++ README.md
@@ -0,0 +1,3 @@
+# adv_basics
+
+Learn Flutter App from the course "Flutter & Dart - The Complete Guide [2023 Edition]" by [Academind](https://academind.com/) on [Udemy](https://Udemy.com/)
---
+++ lib/quiz.dart
@@ -0,0 +1,61 @@
+import 'package:adv_basics/data/questions.dart';
+import 'package:flutter/material.dart';
+import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/questions_screen.dart';
+
+class QuizApp extends StatefulWidget {
+  const QuizApp({super.key});
+
+  @override
+  State<QuizApp> createState() {
+    return _QuizAppState();
+  }
+}
+
+class _QuizAppState extends State<QuizApp> {
+  Widget? activeScreen;
+  final List<String> selectedAnswers = [];
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = QuestionsScreen(
+        onSelectAnswer: chooseAnswer,
+      );
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = StartScreen(switchScreen);
+        selectedAnswers.clear();
+      });
+    }
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple, Colors.purple],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
